53bf4a49c0989213c7df2ca972f96082729d5b6a
jorge298 <jorge.estacio@correounivalle.edu.co>
2024-11-24 01:37:02 -0500

    Carga inicial de los archivos del proyecto

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!--Si se necesita un servicio diferente a vite se necesita construir los archivos necesarios para el servidor con npm run build-->
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        @session('message')
+            <div class="success-message">
+                {{ session('message') }}
+            </div>
+        @endsession
+        {{ $slot }}
+    </body>
+</html>
