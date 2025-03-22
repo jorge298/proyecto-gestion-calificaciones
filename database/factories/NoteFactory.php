1f7ebbee8c5a984627059dc75525e513adcb10d6
germanmurillasdeveloper <german.murillas@correounivalle.edu.co>
2025-03-22 17:16:40 -0500

    Update NoteFactory.php

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class NoteFactory extends Factory
 {
     /**
-     * Define the model's default state.
+     * Define the model's default states.
      *
      * @return array<string, mixed>
      */
