94a8328672b9807ed8798b62b6f8ebce2ec5b25c
germanmurillasdeveloper <german.murillas@correounivalle.edu.co>
2025-03-22 22:19:57 -0500

    inicio de roles y update del composer

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleAndPermissionSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class RoleAndPermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Crear permisos
+        Permission::create(['name' => 'view notes']);
+        Permission::create(['name' => 'edit notes']);
+        Permission::create(['name' => 'delete notes']);
+
+        // Crear roles y asignar permisos
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo(['view notes', 'edit notes', 'delete notes']);
+
+        $userRole = Role::create(['name' => 'user']);
+        $userRole->givePermissionTo(['view notes']);
     }
 }
